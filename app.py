c286ca078e854d487f0bc4c12259e6a18bc75f59
miiiingi <kimmingi722@gmail.com>
2024-01-31 20:11:14 +0900

    fix: merge로 인한 stash 사항 저장

--- app.py
+++ app.py
@@ -5,35 +5,36 @@ from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'database.db')
-
 db = SQLAlchemy(app)
 
-
-class Song(db.Model):
+class Post_DB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    artist = db.Column(db.String(100), nullable=False)
-    title = db.Column(db.String(100), nullable=False)
-    image_url = db.Column(db.String(10000), nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f'{self.username} {self.title} 추천 by {self.username}'
 
-
 with app.app_context():
     db.create_all()
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/newPost')
+@app.route('/NewPost', methods = ['GET', 'POST'])
 def newPost():
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+        # post_db = 
+        print(f'title: {title}')
+        print(f'content: {content}')
     return render_template('newPost.html')
 
 if __name__ == '__main__':
