b7844acdc522f474852176bc9cb8cb24b18efa3f
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-31 18:12:46 +0900

    [feat] 회원가입 필수기능 구현 완료

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -32,8 +32,21 @@ with app.app_context():
 def testMain():
     return render_template('test.html')
 
-@app.route('/account/signup/', endpoint='signup')
+@app.route('/account/signup/', methods=['GET', 'POST'], endpoint='signup')
 def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        userId = request.form['userId']
+        password = request.form['password']
+
+        new_account = Accounts(name=name, email=email, userId=userId, password=password)
+        userDb.session.add(new_account)
+        userDb.session.commit()
+        userDb.session.close()
+
+        return redirect(url_for('testMain')) # 가입 성공 시 원래 화면으로
+
     return render_template('signup.html')
 
 if __name__ == '__main__':  
