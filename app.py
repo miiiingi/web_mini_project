698622caba45b57ef1959283ad77e65852a74298
miiiingi <kimmingi722@gmail.com>
2024-01-31 23:23:02 +0900

    feat: 글쓰기 페이지, 글쓰기 완료 페이지 기본 기능 구현

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from bs4 import BeautifulSoup
-import requests
+import os
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+file_name = 'post_db.db'
+abs_file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'database.db')
+    'sqlite:///' + abs_file_name
 db = SQLAlchemy(app)
 
 class Post_DB(db.Model):
@@ -20,22 +20,28 @@ class Post_DB(db.Model):
         return f'{self.username} {self.title} 추천 by {self.username}'
 
 with app.app_context():
+    if os.path.exists(abs_file_name):
+        db.drop_all()
     db.create_all()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route('/NewPost', methods = ['GET', 'POST'])
+@app.route('/newPost', methods = ['GET', 'POST'])
 def newPost():
     if request.method == "POST":
-        title = request.form['title']
-        content = request.form['content']
-        # post_db = 
-        print(f'title: {title}')
-        print(f'content: {content}')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_db = Post_DB(title = title, content = content)
+        db.session.add(post_db)
+        db.session.commit()
     return render_template('newPost.html')
+
+@app.route('/completePost')
+def completePost():
+    return render_template('completePost.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
