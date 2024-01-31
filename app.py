98ac36554ee5458409b48bb4c5e557c06d13364e
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-31 17:28:15 +0900

    [feat] 회원정보 데이터베이스 제작 세팅

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'userDatabase.db')
+
+userDb = SQLAlchemy(app)
 
 @app.route('/')
 def testMain():
