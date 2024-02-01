c97160e004046fb78874e940460185137db71b24
Eunmi's Macbook Pro <minnie@Eunmisui-MacBookPro.local>
2024-02-01 10:41:13 +0900

    커밋

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+
+# {userId}에 대한 내용이 없어서 그런지 이거 넣으면 오류나서 일단 aoounts로 작성합 
+@app.route("/accounts")
+def accounts():
+    return render_template("mypage.html")
 
 
 if __name__ == "__main__":
